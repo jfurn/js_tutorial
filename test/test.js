bea74e96a1dcaa2a2d53c7e8c3271fa7aabcd643
John Furnborough <john.furnborough@gmail.com>
2019-04-22 13:27:42 +0100

    Add a palindrome-testing web page based on mhartl-palindrome

--- test/test.js
+++ test/test.js
@@ -25,5 +25,9 @@ describe("Phrase", function() {
       let punctuatedPalindrome = new Phrase("Madam, I'm Adam.");
       assert.strictEqual(punctuatedPalindrome.letters(), "MadamImAdam");
     });
+    it("should return the empty string on no match", function() {
+      let noLetters = new Phrase("1234.56");
+      assert.strictEqual(noLetters.letters(), "");
+    });
   });
 });
